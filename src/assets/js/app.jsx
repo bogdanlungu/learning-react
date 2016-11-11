a880dc0b2b3a8f812dc0c609d2363b1af0424e07
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2016-11-11 23:41:57 +0200

    Added some components

--- src/assets/js/app.jsx
+++ src/assets/js/app.jsx
@@ -1,10 +1,51 @@
 /* Define a component
 ----------------------------------- */
-var Main = React.createClass({
+var Button = React.createClass({
+  localHandleClick: function(){
+    this.props.localHandleClick(this.props.increment);
+  },
   render: function() {
     return (
-      <h1>React.js</h1>
+      <button onClick={ this.localHandleClick }>+{ this.props.increment }</button>
     );
+  }
+});
+
+
+/* Another component
+----------------------------------- */
+var Result = React.createClass({
+  render: function(){
+    return(
+      <div>{ this.props.localCounter }</div>
+    )
+  }
+});
+
+
+/* Main component to render more components
+----------------------------------- */
+var Main = React.createClass({
+  getInitialState: function(){
+    return {counter: 0};
+  },
+
+  /* Event handler
+  ----------------------------------- */
+  handleClick: function(increment){
+    this.setState({ counter: this.state.counter + increment });
+  },
+
+  render: function(){
+    return(
+      <div>
+        <Button localHandleClick = {this.handleClick} increment={1} />
+        <Button localHandleClick = {this.handleClick} increment={5} />
+        <Button localHandleClick = {this.handleClick} increment={10} />
+        <Button localHandleClick = {this.handleClick} increment={100} />
+        <Result localCounter = {this.state.counter}/>
+      </div>
+    )
   }
 });
 
